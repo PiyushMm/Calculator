4080972b6ac26b4dbbe3fef81aaf1595c44046b5
Piyush <103035026+8989830820@users.noreply.github.com>
2022-07-30 22:08:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+function display(data){
+    document.querySelector("#input").value += data;
+   }
+   
+   function result(){
+       let ans = document.querySelector("#input").value;
+       let cal = eval(ans);
+       document.querySelector("#input").value = cal;
+   }
+   
+   function clearinput() {
+       document.getElementById("input").value = "";
+   }
