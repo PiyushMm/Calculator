44340b61b07cc9cce03a12d22008f05828d828d3
Piyush <103035026+8989830820@users.noreply.github.com>
2022-09-10 11:45:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function display(data){
-    document.querySelector("#input").value += data;
-   }
-   
-   function result(){
-       let ans = document.querySelector("#input").value;
-       let cal = eval(ans);
-       document.querySelector("#input").value = cal;
-   }
-   
-   function clearinput() {
-       document.getElementById("input").value = "";
-   }
+function dis(val){
+    document.getElementById("input").value += val
+ }
+     
+//function that evaluates the digit and return result
+function solve(){
+    let x = document.getElementById("input").value
+    let y = eval(x)
+    document.getElementById("input").value = y
+ }
+          
+ //function that clear the display
+ function clr()
+ {
+    document.getElementById("input").value = ""
+ }
